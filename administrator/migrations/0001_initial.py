a30a1c7b70ff0f4378ebfa57094edaac069f05c0
seljachauhan23 <seljachauhan23@gmail.com>
2025-01-12 18:36:56 +0530

    some editing
    some editing

--- administrator/migrations/0001_initial.py
+++ administrator/migrations/0001_initial.py
@@ -12,12 +12,14 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Notification',
+            name='ContactUs',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100)),
+                ('email', models.EmailField(max_length=254)),
+                ('subject', models.CharField(max_length=200)),
                 ('message', models.TextField()),
-                ('date_sent', models.DateTimeField(auto_now_add=True)),
-                ('read', models.BooleanField(default=False)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
             ],
         ),
     ]
